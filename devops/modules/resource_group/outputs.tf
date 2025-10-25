6369f411cdae53a66b9d9b5e41c4928f42d71b8b
Abdullah010 <166231259+omega0100@users.noreply.github.com>
2025-10-25 12:30:26 +0300

    Delete devops directory
    يبلا

--- devops/modules/resource_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rg_name" {
-  description = "Resource Group "
-  value       = azurerm_resource_group.rg.name
-}
-
-output "rg_location" {
-  description = "Resource Group location"
-  value       = azurerm_resource_group.rg.location
-}
--- devops/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state-Abdullah-Alotaibi"
-    storage_account_name = "tfstateabdullahalotaibi"
-    container_name       = "tfstate-abdullah-alotaibi"
-    key                  = "dev.terraform.tfstate"
-  }
-}
